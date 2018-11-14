959dda6950f85b8319c7a3b2cfb052bd0dcda3a4
Guilherme Alexandre Camelo Remondes da Silva <a12177@aecastelomaia.pt>
2018-11-14 09:01:11 +0000

    Rule34 Update

--- MasterBot/Core/Comandos/Fun Comands/Rule34.cs
+++ MasterBot/Core/Comandos/Fun Comands/Rule34.cs
@@ -3,21 +3,23 @@ using Discord.Commands;
 using MasterBot.Core.Inteligencia;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace MasterBot.Core.Comandos
 {
-    class Rule34 : ModuleBase<SocketCommandContext>
+    public class Rule34 : ModuleBase<SocketCommandContext>
     {
-        /*
+        
         //https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags={}
         [Command("Rule34"), Alias("rule34", "regra34", "Regra34", "REGRA34")]
-        public async Task Rule34(string tags)
+        public async Task Rule34_Comand(string tags)
         {
             XmlDocument xml = new XmlDocument();
-            xml.LoadXml(https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=mite); // suppose that myXmlString contains "<Names>...</Names>"
+            var wc = new WebClient();
+            xml.LoadXml(wc.DownloadString("https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=mite"));
 
             XmlNodeList xnList = xml.SelectNodes("/Names/Name");
             foreach (XmlNode xn in xnList)
@@ -31,6 +33,6 @@ namespace MasterBot.Core.Comandos
             embed.WithImageUrl("https://cdn.discordapp.com/attachments/430797829078908928/441255069518921728/Lvl40raid.png");
 
             await ReplyAsync("", false, embed);
-        */}
+        }
     }
 }
