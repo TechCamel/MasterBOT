40c5815f5210560499a0b47ebae37e4e3522d47a
miure <gui.remondes@gmail.com>
2018-11-17 00:05:15 +0000

    Anuncios e verdade e consequencia reformolação de ficheiros

---
+++ MasterBot/Core/Comandos/Anuncios.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterBot.Core.Comandos
+{
+    class Anuncios : ModuleBase<SocketCommandContext>
+    {/*
+        [Command("hello"), Alias("Hello World", "hello world", "hello World", "Hello World", "HELLO WORLD")]
+        public async Task hello()
+        {
+            SocketUser user = Context.User;
+            StreamReader amizade = new StreamReader($@"{Program.path}\Data\Users\{user.Username}.txt");
+            amizade.ReadLine();
+            int LVL_amizade = Convert.ToInt32(amizade.ReadLine().ToString());
+            if (LVL_amizade != 0)
+                await Context.Channel.SendMessageAsync($"{Context.User.Mention} Hello World");
+            else
+                await Context.Channel.SendMessageAsync($"Mas eu conheço-te?? {Context.User.Mention}");
+        }*/
+    }
+}
---
+++ MasterBot/Core/Comandos/Fun Comands/Verdade ou Consequencia.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterBot.Core.Comandos.Fun_Comands
+{
+    class Class1
+    {
+    }
+}
